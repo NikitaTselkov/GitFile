0f11318bd53d85999e3e80f8e044b391bbca1537
Nikita <ncelkov@rit-it.com>
2022-02-03 10:56:03 +0700

    Добавьте файлы проекта.

---
+++ GitFile/GitCommand.cs
@@ -0,0 +1,95 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace GitFile
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class GitCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("3025a1db-e889-4caa-ad24-006760fbb4f9");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GitCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private GitCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.StartAllGitFiles, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static GitCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new GitCommand(package, commandService);
+        }
+
+        private void StartAllGitFiles(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE dte = Package.GetGlobalService(typeof(SDTE)) as DTE;
+            var path = Path.GetDirectoryName(dte.Solution.FullName);
+
+            foreach (var filePath in Directory.EnumerateFiles(path, "*.git", SearchOption.AllDirectories))
+            {
+                GitCompiler.StartGitFile(filePath);
+            }
+        }
+    }
+}
---
+++ GitFile/GitCompiler.cs
@@ -0,0 +1,63 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFile
+{
+    public static class GitCompiler
+    {
+        private static string CommandTitle { get; set; }
+        private static string CommandLine { get; set; }
+
+        public static void StartGitFile(string filePath)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            DTE dte = Package.GetGlobalService(typeof(SDTE)) as DTE;
+            DTE2 dte2 = (DTE2)dte;
+            var p = new System.Diagnostics.Process();
+            string line;
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine().Trim();
+
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        if (line.Contains("."))
+                        {
+                            CommandTitle = line.Replace(".", "");
+                            CommandLine = CommandTitle + " ";
+                        }
+                        else
+                        {
+                            CommandLine += line;
+
+                            p.StartInfo.UseShellExecute = false;
+                            p.StartInfo.FileName = "cmd.exe";
+                            p.StartInfo.Arguments = "/C " + CommandLine;
+                            p.StartInfo.CreateNoWindow = true;
+                            p.StartInfo.RedirectStandardOutput = true;
+                            p.Start();
+
+                            string output = p.StandardOutput.ReadToEnd();
+
+                            dte2.ToolWindows.CommandWindow.OutputString(output);
+
+                            CommandLine = CommandTitle + " ";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
